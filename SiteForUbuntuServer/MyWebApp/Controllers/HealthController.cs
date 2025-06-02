13182d47f6f2280570a289c130dae1e67d1deb6d
RUTMIIT monstro <60342619+namek1305@users.noreply.github.com>
2025-06-02 15:25:38 +0300

    Delete SiteForUbuntuServer/MyWebApp directory

--- SiteForUbuntuServer/MyWebApp/Controllers/HealthController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-[ApiController]
-[Route("[controller]")]
-public class HealthController : ControllerBase
-{
-    [HttpGet]
-    public IActionResult Check() => Ok("AnimeAPI is online ヾ(^▽^*)))");
-}
