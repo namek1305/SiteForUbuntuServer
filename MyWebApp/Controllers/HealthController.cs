52d670b916c1c75fe36fb8ad181c9e55729a87a7
RUTMIIT monstro <60342619+namek1305@users.noreply.github.com>
2025-06-02 15:06:47 +0300

    part 2

---
+++ MyWebApp/Controllers/HealthController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Check() => Ok("AnimeAPI is online ヾ(^▽^*)))");
+}
